dc1b7e84d96455ff6f06d53cc5b02948eecc91ea
James Marotta <james.marotta@outlook.com>
2020-08-14 21:32:43 +1000

    tweets are a separate api

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,31 +5,30 @@ const Twit = require('twit');
 require('dotenv').config();
 
 // types
-import { Request, Response, NextFunction } from 'express';
-import { User as TwitterUser } from 'twitter-d';
-import { IncomingMessage } from 'http';
+import { Request, Response, NextFunction } from 'express'
+import { User as TwitterUser } from 'twitter-d'
+import { IncomingMessage } from 'http'
 
 const T = new Twit({
   consumer_key: process.env.API_KEY as string,
   consumer_secret: process.env.API_SECRET_KEY as string,
   access_token: process.env.ACCESS_TOKEN as string,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET as string,
-});
+})
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Send a 500 otherwise keep going
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((_req: Request, res: Response, next: NextFunction) => {
   try {
-    next();
+    next()
   } catch (e) {
-    res.sendStatus(500);
+    res.sendStatus(500)
   }
-});
-
+})
 
 app.get('/search', async (req: Request, res: Response) => {
   const query = req.query.q as string 
@@ -37,49 +36,45 @@ app.get('/search', async (req: Request, res: Response) => {
   T.get(
     'users/search',
     { q: query },
-    (err: Error, data: TwitterUser[], response: IncomingMessage) => {
+    (err: Error, data:TwitterUser[], response: IncomingMessage) => {
       if (
         err ||
         (response.statusCode &&
           (response.statusCode >= 200 || response.statusCode <= 300))
       ) {
-        console.error('there was an error hitting the twitter Api');
+        console.error('there was an error hitting the twitter Api')
       }
 
-      res.send(data);
+      res.send(data)
     },
-  );
-});
+  )
+})
 
-
-type ourParams = { user_id: string, screen_name: string} 
+type ourParams = { user_id: string; screen_name: string }
 
 app.get('/users/:id/:screen_name', async (req: Request, res: Response) => {
-  const {user_id, screen_name}: ourParams = req.params as ourParams
+  const { user_id, screen_name }: ourParams = req.params as ourParams
    
-  
   T.get(
     'users/show',
     { id: user_id, screen_name },
-    (err: Error, data: TwitterUser, response: IncomingMessage) => {
+    (err: Error, data:TwitterUser, response: IncomingMessage) => {
       if (
         err ||
         (response.statusCode &&
           (response.statusCode >= 200 || response.statusCode <= 300))
       ) {
-        console.error('there was an error hitting the twitter Api');
+        console.error('there was an error hitting the twitter Api')
       }
 
-      res.send(data);
+      res.send(data)
     },
-  );
-});
+  )
+})
 
-
-
-
-app.listen(4000);
+app.listen(4000)
 console.log(
   `Listening on port: 4000, wait for the development server to be up...`,
-);
+)
 
+export default app
